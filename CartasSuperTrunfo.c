8308969dd40b474eca893b12547dffd2927ba190
Diogovitor8 <diogotrompt@gmail.com>
2025-03-02 08:02:52 +0000

    Adicionado densidade e pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,17 +5,19 @@ int main() {
     printf("Olá, vamos comerçar!\n");
    
     char cidade[50];
-    int codigo;
+    char codigo[50];
     int turismo;
     float populacao;
     int area;
     float pib;
+    float divisao = populacao / area; 
+    float percapita = pib / populacao;
 
     printf("Digite o nome da cidade:\n");
     scanf("%s" , &cidade);
 
     printf("Digite o codigo da cidade:\n");
-    scanf("%d", &codigo);
+    scanf("%s", &codigo);
 
     printf("Quantidade de locais turisticos:\n");
     scanf("%d" , &turismo);   
@@ -30,9 +32,11 @@ int main() {
     scanf("%f" , &pib);
 
     printf("Dados cadastrados, vamos lá:\n");
-    printf(" Sua cidade: %s\n" , cidade);
-    printf(" Código: %d\n Locais turisticos: %d\n" , codigo, turismo);
-    printf(" População: %f\n Área da cidade: %d\n Pib: %f\n" , populacao, area, pib);
+    printf(" Sua cidade: %s\n Código: %s\n" , cidade, codigo);
+    printf(" Locais turisticos: %d\n" , turismo);
+    printf(" População: %.3f\n Área da cidade: %d\n Pib: %f\n" , populacao, area, pib);
+    printf(" Densidade populacional: %.2f\n" , populacao / area);
+    printf(" PIB per capita: %.2f\n" , pib / populacao);
 
     return 0;
 }
